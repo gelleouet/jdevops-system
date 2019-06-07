aa34a7bcaa623b7a42cff20e3de13418bd4076ad
gelleouet <gregory.elleouet@gmail.com>
2019-06-07 16:31:25 +0200

    Ajout script/config pour backup de la base

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # smarthome-system
-Script system 
+
+Outils d'installation et de configuration d'une instance jdevops
+
+## Firewall
+
+## AWS CLI
+
+Installation python3 et pip
+
+    apt-get install python3
+    apt-get install python3-pip
+
+Installation aws cli  
+https://docs.aws.amazon.com/fr_fr/cli/latest/userguide/install-linux.html
+
+    pip3 install awscli --upgrade --user
+    
+    
---
+++ scripts/backup-postgresql.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Script pour faire des dump d'une base
+#
+# $1 dbname : nom de la base
+#
+
+# gestion des parametres
+DBNAME="$1"
+
+if [ -z "$DBNAME" ]
+then
+	echo "Usage : $0 dbname"
+	exit 1
+fi
+
+# configuration
+PATH_DUMP="/tmp"
+PG_DUMP="/usr/bin/pg_dump"
+DATE_JOUR=`date +%Y-%m-%d`
+PGCONNECT_TIMEOUT=10
+
+
+# Dump de la base 
+$PG_DUMP -U postgres -h localhost --format=c --file=$PATH_DUMP/${DBNAME}_$DATE_JOUR.backup $DBNAME"
+
